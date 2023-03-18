9952590606133dcbd102b9780351807d03061584
Ritik <ritik151112@gmail.com>
2023-03-18 21:03:33 +0530

    InitailCommint

---
+++ src/portfolio.js
@@ -0,0 +1,469 @@
+/* Change this file to get your personal Porfolio */
+
+// Website related settings
+const settings = {
+  isSplash: true, // Change this to false if you don't want Splash screen.
+};
+
+//SEO Related settings
+const seo = {
+  title: "Ritik's Portfolio",
+  description:
+    "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
+  og: {
+    title: "Ritik Kumar Portfolio",
+    type: "website",
+    url: "http://ritikkumar.com/",
+  },
+};
+
+//Home Page
+const greeting = {
+  title: "Ritik Kumar",
+  logo_name: "RitikKumar",
+  nickname: "love to code",
+  subTitle:
+    "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
+  resumeLink: "https://app.luminpdf.com/viewer/6415c0dc0533a468ca067ce9",
+  portfolio_repository: "#",
+  githubProfile: "https://github.com/Ritikkumar992",
+};
+
+const socialMediaLinks = [
+  {
+    name: "Github",
+    link: "https://github.com/Ritikkumar992",
+    fontAwesomeIcon: "fa-github", // Reference https://fontawesome.com/icons/github?style=brands
+    backgroundColor: "#181717", // Reference https://simpleicons.org/?q=github
+  },
+  {
+    name: "LinkedIn",
+    link: "https://www.linkedin.com/in/ritik-kumar-3039091ba/",
+    fontAwesomeIcon: "fa-linkedin-in", // Reference https://fontawesome.com/icons/linkedin-in?style=brands
+    backgroundColor: "#0077B5", // Reference https://simpleicons.org/?q=linkedin
+  },
+  {
+    name: "Gmail",
+    link: "mailto:ritik.email01@gmail.com",
+    fontAwesomeIcon: "fa-google", // Reference https://fontawesome.com/icons/google?style=brands
+    backgroundColor: "#D14836", // Reference https://simpleicons.org/?q=gmail
+  },
+  {
+    name: "Twitter",
+    link: "https://twitter.com/Ritikku59394466",
+    fontAwesomeIcon: "fa-twitter", // Reference https://fontawesome.com/icons/twitter?style=brands
+    backgroundColor: "#1DA1F2", // Reference https://simpleicons.org/?q=twitter
+  },
+  {
+    name: "Facebook",
+    link: "https://www.facebook.com/ritik.shrivastav.56232/",
+    fontAwesomeIcon: "fa-facebook-f", // Reference https://fontawesome.com/icons/facebook-f?style=brands
+    backgroundColor: "#1877F2", // Reference https://simpleicons.org/?q=facebook
+  },
+  {
+    name: "Instagram",
+    link: "https://www.instagram.com/__ritik_srivastav/",
+    fontAwesomeIcon: "fa-instagram", // Reference https://fontawesome.com/icons/instagram?style=brands
+    backgroundColor: "#E4405F", // Reference https://simpleicons.org/?q=instagram
+  },
+];
+
+const skills = {
+  data: [
+    {
+      title: "Full Stack Development",
+      fileName: "FullStackImg",
+      skills: [
+        "⚡ Building responsive website front end using React-Redux",
+        "⚡ Developing mobile applications using Flutter, React Native and solo android apps using Kotlin",
+        "⚡ Creating application backend in Node, Express & Flask",
+      ],
+      softwareSkills: [
+        {
+          skillName: "HTML5",
+          fontAwesomeClassname: "simple-icons:html5",
+          style: {
+            color: "#E34F26",
+          },
+        },
+        {
+          skillName: "CSS3",
+          fontAwesomeClassname: "fa-css3",
+          style: {
+            color: "#1572B6",
+          },
+        },
+        {
+          skillName: "Sass",
+          fontAwesomeClassname: "simple-icons:sass",
+          style: {
+            color: "#CC6699",
+          },
+        },
+        {
+          skillName: "JavaScript",
+          fontAwesomeClassname: "simple-icons:javascript",
+          style: {
+            backgroundColor: "#000000",
+            color: "#F7DF1E",
+          },
+        },
+        {
+          skillName: "ReactJS",
+          fontAwesomeClassname: "simple-icons:react",
+          style: {
+            color: "#61DAFB",
+          },
+        },
+        {
+          skillName: "NodeJS",
+          fontAwesomeClassname: "simple-icons:node-dot-js",
+          style: {
+            color: "#339933",
+          },
+        },
+        {
+          skillName: "NPM",
+          fontAwesomeClassname: "simple-icons:npm",
+          style: {
+            color: "#CB3837",
+          },
+        },
+        {
+          skillName: "Yarn",
+          fontAwesomeClassname: "simple-icons:yarn",
+          style: {
+            color: "#2C8EBB",
+          },
+        },
+      ],
+    },
+  ],
+};
+
+// Education Page
+const competitiveSites = {
+  competitiveSites: [
+    {
+      siteName: "LeetCode",
+      iconifyClassname: "simple-icons:leetcode",
+      style: {
+        color: "#F79F1B",
+      },
+      profileLink: "https://leetcode.com/ritik151112/",
+    },
+    {
+      siteName: "Codechef",
+      iconifyClassname: "simple-icons:codechef",
+      style: {
+        color: "#5B4638",
+      },
+      profileLink: "https://www.codechef.com/users/ritik_kr1726",
+    },
+    {
+      siteName: "Codeforces",
+      iconifyClassname: "simple-icons:codeforces",
+      style: {
+        color: "#1F8ACB",
+      },
+      profileLink: "https://codeforces.com/profile/ritik151112",
+    },
+    {
+      siteName: "Hackerearth",
+      iconifyClassname: "simple-icons:hackerearth",
+      style: {
+        color: "#323754",
+      },
+      profileLink: "https://www.hackerrank.com/ritik151112",
+    },
+  ],
+};
+
+const degrees = {
+  degrees: [
+    {
+      title: "Budge Budge Institute of Technology Kolkata West Bengal",
+      subtitle: "B.Tech. in Computer Engineering",
+      logo_path: "bbit.jpg",
+      alt_name: "BBIT Kolkata",
+      duration: "2020 - 2024",
+      descriptions: [
+        "⚡ SGPA(Aggregate) : 9.02",
+        "⚡ SGPA(5th semester) : 7.63",
+        "⚡ My College life is a roller coaster ride that you'll never want to get off!.",
+        "⚡ My College life is an adventure that challenges me to step out of my comfort zone and try new things!.",
+      ],
+      website_link: "https://www.bbit.edu.in/",
+    },
+    {
+      title: "Jawahar Navodaya Vidyalaya Muzaffarpur Bihar",
+      subtitle: "Senior Secondary",
+      logo_path: "jnv-logo.png",
+      alt_name: "JNV Muzaffarpur",
+      duration: "2018 - 2020",
+      descriptions: [
+        "⚡ Percentage : 82.5% scored",
+        "⚡ Jnv taught me get fun without drink , talk without cellphone , dream without drugs , smile without selfies.",
+        "⚡ In short it's a heaven where u learn everything to survive in life...",
+      ],
+      website_link: "https://navodaya.gov.in/nvs/en/Home1/",
+    },
+    // {
+    //   title: "DAV Public School Malight Muzaffarpur Bihar",
+    //   subtitle: "Secondary ",
+    //   logo_path: "dav-logo.jpg",
+    //   alt_name: "DAV Muzaffarpur",
+    //   duration: "2016 - 2018",
+    //   descriptions: [
+    //     "⚡ I have taken varity of courses related to Artificial Intelligence which correspond to Explainable AI, Graph Machine Learning, Computer Vision etc.",
+    //     "⚡ Apart from this, I have also done research assistantship. As part of it, I have worked on creating new algorithms in Graph ML and Network Science.",
+    //     "⚡ During my time at university, I was also associated with multimedia department. As part of it, I have worked on some documentry films and interviews.",
+    //   ],
+    //   website_link: "#",
+    // },
+  ],
+};
+
+// const certifications = {
+//   certifications: [
+//     {
+//       title: "Machine Learning",
+//       subtitle: "- Andrew Ng",
+//       logo_path: "stanford_logo.png",
+//       certificate_link:
+//         "https://www.coursera.org/account/accomplishments/verify/22MTSSC5WDTM",
+//       alt_name: "Stanford University",
+//       color_code: "#8C151599",
+//     },
+//     {
+//       title: "Deep Learning",
+//       subtitle: "- Andrew Ng",
+//       logo_path: "deeplearning_ai_logo.png",
+//       certificate_link:
+//         "https://www.coursera.org/account/accomplishments/specialization/H8CPSFXAJD2G",
+//       alt_name: "deeplearning.ai",
+//       color_code: "#00000099",
+//     },
+//     {
+//       title: "ML on GCP",
+//       subtitle: "- GCP Training",
+//       logo_path: "google_logo.png",
+//       certificate_link:
+//         "https://www.coursera.org/account/accomplishments/specialization/EB4VJARK8647",
+//       alt_name: "Google",
+//       color_code: "#0C9D5899",
+//     },
+//     {
+//       title: "Data Science",
+//       subtitle: "- Alex Aklson",
+//       logo_path: "ibm_logo.png",
+//       certificate_link:
+//         "https://www.coursera.org/account/accomplishments/specialization/PLEAPCSJBZT5",
+//       alt_name: "IBM",
+//       color_code: "#1F70C199",
+//     },
+//     {
+//       title: "Big Data",
+//       subtitle: "- Kim Akers",
+//       logo_path: "microsoft_logo.png",
+//       certificate_link:
+//         "https://drive.google.com/file/d/164zKCFOsI4vGqokc-Qj-e_D00kLDHIrG/view",
+//       alt_name: "Microsoft",
+//       color_code: "#D83B0199",
+//     },
+//     {
+//       title: "Advanced Data Science",
+//       subtitle: "- Romeo Kienzler",
+//       logo_path: "ibm_logo.png",
+//       certificate_link:
+//         "https://www.coursera.org/account/accomplishments/verify/BH2T9BRU87BH",
+//       alt_name: "IBM",
+//       color_code: "#1F70C199",
+//     },
+//     {
+//       title: "Advanced ML on GCP",
+//       subtitle: "- GCP Training",
+//       logo_path: "google_logo.png",
+//       certificate_link:
+//         "https://www.coursera.org/account/accomplishments/verify/5JZZM7TNQ2AV",
+//       alt_name: "Google",
+//       color_code: "#0C9D5899",
+//     },
+//     {
+//       title: "DL on Tensorflow",
+//       subtitle: "- Laurence Moroney",
+//       logo_path: "deeplearning_ai_logo.png",
+//       certificate_link:
+//         "https://www.coursera.org/account/accomplishments/verify/6T4DCUGNK8J8",
+//       alt_name: "deeplearning.ai",
+//       color_code: "#00000099",
+//     },
+//     {
+//       title: "Fullstack Development",
+//       subtitle: "- Jogesh Muppala",
+//       logo_path: "coursera_logo.png",
+//       certificate_link:
+//         "https://www.coursera.org/account/accomplishments/certificate/NRANJA66Y2YA",
+//       alt_name: "Coursera",
+//       color_code: "#2A73CC",
+//     },
+//     {
+//       title: "Kuberenetes on GCP",
+//       subtitle: "- Qwiklabs",
+//       logo_path: "gcp_logo.png",
+//       certificate_link:
+//         "https://google.qwiklabs.com/public_profiles/e4d5a92b-faf6-4679-a70b-a9047c0cd750",
+//       alt_name: "GCP",
+//       color_code: "#4285F499",
+//     },
+//     {
+//       title: "Cryptography",
+//       subtitle: "- Saurabh Mukhopadhyay",
+//       logo_path: "nptel_logo.png",
+//       certificate_link:
+//         "https://drive.google.com/open?id=1z5ExD_QJVdU0slLkp8CBqSF3-C3g-ro_",
+//       alt_name: "NPTEL",
+//       color_code: "#FFBB0099",
+//     },
+//     {
+//       title: "Cloud Architecture",
+//       subtitle: "- Qwiklabs",
+//       logo_path: "gcp_logo.png",
+//       certificate_link:
+//         "https://google.qwiklabs.com/public_profiles/5fab4b2d-be6f-408c-8dcb-6d3b58ecb4a2",
+//       alt_name: "GCP",
+//       color_code: "#4285F499",
+//     },
+//   ],
+// };
+
+// Experience Page
+const experience = {
+  title: "Experience",
+  subtitle: "Work, Internship and Volunteership",
+  description:
+    "I have worked with many evolving startups as a frontend Developer. I have also worked with some frelanced and service providers companies  as Frontend Developer. I love participating in  events and that is why I am also involved with many coding communities as a representative.",
+  header_image_path: "experience.svg",
+  sections: [
+    {
+      title: "Internships",
+      experiences: [
+        {
+          title: "Frontend Development Intern",
+          company: "ActibeWeb.",
+          company_url: "https://app.luminpdf.com/viewer/6415d63f0533a468ca09fc94",
+          logo_path: "actibeWeb-logo.jpeg",
+          duration: "Aug 2022 - Nov 2022",
+          location: "Remote",
+          description:
+            "Ensure user satisfaction and retention by providing responsive websites. Build and maintain software documentation sites using various programming languages. Increase productivity by using software to organize, track bug patches and add feature requests. Created and tested applications for websites having a large impact on the design, architecture, and development of consumer products. Work with other team members to develop the architecture and design of new and current system. TechStack : React, JS, Bootstrap.",
+          color: "#000000",
+        }
+      ],
+    },
+  ],
+};
+
+// Projects Page
+const projectsHeader = {
+  title: "Projects",
+  description:
+    "My projects makes use of vast variety of latest technology tools. My best experience is to create Data Science projects and deploy them to web applications using cloud infrastructure.",
+  avatar_image_path: "projects_image.svg",
+};
+
+// const publicationsHeader = {
+//   title: "Publications",
+//   description:
+//     "I have worked on and published a few research papers and publications of my own.",
+//   avatar_image_path: "projects_image.svg",
+// };
+
+// const publications = {
+//   data: [
+//     {
+//       id: "MDEwOlJlcG9zaXRvcnkyNDU0NjcyNzQ=",
+//       name: "Artificial Intelligence Paper",
+//       createdAt: "2020-03-06T16:26:54Z",
+//       description: "Paper Written on Artificial Intelligence published in xyz ",
+//       url:
+//         "https://www.andrewng.org/publications/building-high-level-features-using-large-scale-unsupervised-learning/",
+//     },
+//     {
+//       id: "MDEwOlJlcG9zaXRvcnkyNDU0NjcyNzi=",
+//       name: "Artificial Intelligence Paper",
+//       createdAt: "2020-03-06T16:26:54Z",
+//       description: "Paper Written on Artificial Intelligence published in xyz ",
+//       url:
+//         "https://www.andrewng.org/publications/building-high-level-features-using-large-scale-unsupervised-learning/",
+//     },
+//     {
+//       id: "MDEwOlJlcG9zaXRvcnkyNDU0NjcyNze=",
+//       name: "Artificial Intelligence Paper",
+//       createdAt: "2020-03-06T16:26:54Z",
+//       description: "Paper Written on Artificial Intelligence published in xyz ",
+//       url:
+//         "https://www.andrewng.org/publications/building-high-level-features-using-large-scale-unsupervised-learning/",
+//     },
+//     {
+//       id: "MDEwOlJlcG9zaXRvcnkyNDU0NjcyNzt=",
+//       name: "Artificial Intelligence Paper",
+//       createdAt: "2020-03-06T16:26:54Z",
+//       description: "Paper Written on Artificial Intelligence published in xyz ",
+//       url:
+//         "https://www.andrewng.org/publications/building-high-level-features-using-large-scale-unsupervised-learning/",
+//     },
+//     {
+//       id: "MDEwOlJlcG9zaXRvcnkyNDU0NjcyNzb=",
+//       name: "Artificial Intelligence Paper",
+//       createdAt: "2020-03-06T16:26:54Z",
+//       description: "Paper Written on Artificial Intelligence published in xyz ",
+//       url:
+//         "https://www.andrewng.org/publications/building-high-level-features-using-large-scale-unsupervised-learning/",
+//     },
+//   ],
+// };
+
+// Contact Page
+const contactPageData = {
+  contactSection: {
+    title: "Contact Me",
+    profile_image_path: "profile_01.jpg",
+    description:
+      "I am available on almost every social media. You can message me, I will reply within 24 hours. I can help you with React, JS, Node ,DataBases and many more.",
+  },
+  blogSection: {
+    title: "Blogs",
+    subtitle:
+      "For individual fundamental empowerment, I like to write powerful lessons that create impact on each of the reader individually to change the core of their character.",
+    link: "#",
+    avatar_image_path: "blogs_image.svg",
+  },
+  addressSection: {
+    title: "Address",
+    subtitle:
+      "Nishchintapur, Budge Budge, Kolkata, West Bengal 700137",
+    avatar_image_path: "address_image.svg",
+    location_map_link: "https://goo.gl/maps/Zo4xkU7zWy4cMJVa6",
+  },
+  phoneSection: {
+    title: "Phone no",
+    subtitle: "9693882391",
+  },
+};
+
+export {
+  settings,
+  seo,
+  greeting,
+  socialMediaLinks,
+  skills,
+  competitiveSites,
+  degrees,
+  // certifications,
+  experience,
+  projectsHeader,
+  // publicationsHeader,
+  // publications,
+  contactPageData,
+};
